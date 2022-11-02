6ed440336ca75d010fec503df565df34f0fb9873
Duck1s <32653@ma-web.nl>
2022-11-02 22:32:30 +0100

    feat(save): trying to save statistics in global state and json modification

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 // Functions
 function App({ Component, pageProps }: AppProps) {
+  const [statistics, setStatistics] = useState([]);
+
   return (
     <>
       <Layout>
-        <Component {...pageProps} />
+        <Component
+          {...pageProps}
+          statistics={statistics}
+          setStatistics={setStatistics}
+        />
       </Layout>
     </>
   );
--- pages/questions/[id].tsx
+++ pages/questions/[id].tsx
@@ -1,17 +1,27 @@
 // Imports
 import questions from "../../public/questions.json";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 // Functions
-function QuestionPage({ question }: any) {
+function QuestionPage({ question, statistics, setStatistics }: any) {
   // Router
   const router = useRouter();
+
+  // Set Statistics
+  setStatistics(questions);
+
+  // State
+  const [score, setScore] = useState(question.rating);
 
   // Handle Prev
   const handlePrev = () => {
     if (question.number > 1) {
+      handleSubmit();
       router.push(`/questions/${question.number - 1}`);
     } else {
+      handleSubmit();
       router.push("/");
     }
   };
@@ -19,10 +29,34 @@ function QuestionPage({ question }: any) {
   // Handle Next
   const handleNext = () => {
     if (question.number < questions.length) {
+      handleSubmit();
+
       router.push(`/questions/${question.number + 1}`);
     } else {
+      handleFinish();
       router.push("/results");
     }
+  };
+
+  // Handle Submit
+  const handleSubmit = () => {
+    question.rating = score;
+    setStatistics(...statistics, question);
+    console.log(statistics);
+
+    setScore(0);
+  };
+
+  // Handle Finish
+  const handleFinish = () => {};
+
+  // Handle Score
+  const handlePlus = () => {
+    if (score < 5) setScore(score + 1);
+  };
+
+  const handleMinus = () => {
+    if (score > 0) setScore(score - 1);
   };
 
   return (
@@ -30,6 +64,18 @@ function QuestionPage({ question }: any) {
       <section>
         <h1 className="text-2xl font-bold mb-2">Question Page</h1>
         <p className="mb-8">{question.question}</p>
+        <section className="mb-8">
+          <p>Score</p>
+          <div className="text-3xl font-bold mb-6">{score}</div>
+          <div className="flex gap-4 justify-center items-center">
+            <button>
+              <FaMinus onClick={handleMinus} />
+            </button>
+            <button onClick={handlePlus}>
+              <FaPlus />
+            </button>
+          </div>
+        </section>
         <div className="inline-flex">
           <button
             onClick={handlePrev}
@@ -57,7 +103,6 @@ export async function getServerSideProps(context: any) {
   const contextid = context.params;
   const questionid = contextid.id - 1;
   const question = questions[questionid];
-  console.log(question);
 
   return {
     props: {
