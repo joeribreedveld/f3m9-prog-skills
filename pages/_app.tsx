13fd1cb4b61772090daff150205acc36346b872b
Duck1s <32653@ma-web.nl>
2022-11-02 20:28:39 +0100

    clean: remove unused files and install tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// Imports
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Functions
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+// Exports
+export default App;
